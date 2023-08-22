f464135120431ee9e558c4ec3d910fbf9c71c380
lawanaji <lawanmustaphaaji2015@gmail.com>
2023-08-22 09:17:35 +0100

    make changies on button

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const remEl = document.querySelector('.x-code');
 const menuCard = document.querySelector('.menu-card')
 const hamburger =  document.querySelector('.hamburger')
 
-function toggle(){
-    if(menuCard.style.display === 'none' ){
+function toggle(menuCard){
+    if(menuCard.style.display == 'none'){
         menuCard.style.display = 'block';
     }else{
-        menuCard.style.display = 'none';
+        menuCard.style.display = 'none'
     }
 }
-toggle()
+toggle(menuCard)
 
 
 // console.log(hamburger)
